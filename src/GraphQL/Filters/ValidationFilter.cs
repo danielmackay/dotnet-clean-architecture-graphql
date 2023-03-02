1597346805db9861a3bac2f64e51dfcc5b7c3922
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-03-02 22:25:45 +1000

    Added support for multiple validation errors per field

--- src/GraphQL/Filters/ValidationFilter.cs
+++ src/GraphQL/Filters/ValidationFilter.cs
@@ -8,10 +8,10 @@ public class ValidationFilter : IErrorFilter
     {
         if (error.Exception is not null and ValidationException ex)
         {
+            // NOTE: Is there a tidier way to to this?
             var errors = new Dictionary<string, object?>();
             foreach (var key in ex.Errors.Keys)
-                foreach (var item in ex.Errors[key])
-                    errors.Add(key, item);
+                errors.Add(key, ex.Errors[key]);
 
             return error
                 .WithMessage(ex.Message)
