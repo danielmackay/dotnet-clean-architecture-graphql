639db868130112af56e93ed7b27fe6dada1a2bd8
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-03-04 07:34:56 +1000

    Added weather forecast query

--- src/GraphQL/Types/Query.cs
+++ src/GraphQL/Types/Query.cs
@@ -1,5 +1,7 @@
-﻿using CA.GraphQL.Domain.Entities;
+﻿using CA.GraphQL.Application.WeatherForecasts.Queries.GetWeatherForecasts;
+using CA.GraphQL.Domain.Entities;
 using CA.GraphQL.Infrastructure.Persistence;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace GraphQL.Types;
@@ -17,4 +19,10 @@ public class Query
     [UseFiltering]
     [UseSorting]
     public IQueryable<TodoList> GetTodoLists([Service(ServiceKind.Synchronized)] ApplicationDbContext dbContext) => dbContext.TodoLists.AsNoTracking();
+
+    [UseOffsetPaging(IncludeTotalCount = true)]
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    public async Task<IEnumerable<WeatherForecast>> GetWeatherForecasts([Service] ISender sender) => await sender.Send(new GetWeatherForecastsQuery());
 }
