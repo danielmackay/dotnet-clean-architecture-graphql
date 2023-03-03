72afdfcce7bc8e6ef6dcac618e7856256e7bf410
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-03-04 08:40:20 +1000

    ✨ Add better handling for errors (#8)

--- src/GraphQL/Filters/ValidationFilter.cs
+++ src/GraphQL/Filters/ValidationFilter.cs
@@ -6,18 +6,35 @@ public class ValidationFilter : IErrorFilter
 {
     public IError OnError(IError error)
     {
-        if (error.Exception is not null and ValidationException ex)
+        if (error.Exception is not null and ValidationException valEx)
         {
             // NOTE: Is there a tidier way to to this?
             var errors = new Dictionary<string, object?>();
-            foreach (var key in ex.Errors.Keys)
-                errors.Add(key, ex.Errors[key]);
+            foreach (var key in valEx.Errors.Keys)
+                errors.Add(key, valEx.Errors[key]);
 
             return error
-                .WithMessage(ex.Message)
+                .WithCode(ErrorCode.ClientError.ToString())
+                .WithMessage(valEx.Message)
                 .WithExtensions(errors);
+        }
+
+        if (error.Exception is not null and NotFoundException nfEx)
+        {
+            return error
+                .RemoveExtensions()
+                .WithCode(ErrorCode.NotFound.ToString())
+                .WithMessage(nfEx.Message);
         }
 
         return error;
     }
+}
+
+public enum ErrorCode
+{
+    NoError = 0,
+    ClientError = 400,
+    NotFound = 404,
+    ServerError = 500,
 }
