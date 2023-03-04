145c40c5df3a9b23ce393b858e531353e46e7ef8
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-03-04 10:27:52 +1000

    ✨ Added basic blazor FE (#9)

---
+++ src/BlazorUI/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorUI;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services
+    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services
+    .AddTodoClient(strategy: StrawberryShake.ExecutionStrategy.CacheAndNetwork)
+    .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://localhost:7114/graphql/"));
+
+await builder.Build().RunAsync();
