68a09b4f04c326e21ec46f8469087301e0a12d0c
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-03-10 08:01:17 +1000

    Added backend subscription for todo item created

--- src/GraphQL/Mutations/TodoItemMutation.cs
+++ src/GraphQL/Mutations/TodoItemMutation.cs
@@ -4,14 +4,22 @@ using CA.GraphQL.Application.TodoItems.Commands.DeleteTodoItem;
 using CA.GraphQL.Application.TodoItems.Commands.PurgeTodoItems;
 using CA.GraphQL.Application.TodoItems.Commands.UpdateTodoItem;
 using CA.GraphQL.Application.TodoItems.Commands.UpdateTodoItemDetail;
+using HotChocolate.Subscriptions;
 using MediatR;
 
 namespace GraphQL.Mutations;
 
+public record TodoItemCreatedPayload(int TodoItemId);
+
 [MutationType]
 public class TodoItemMutation
 {
-    public async Task<CreateTodoItemPayload> CreateTodoItem(CreateTodoItemCommand input, [Service] ISender sender) => await sender.Send(input);
+    public async Task<CreateTodoItemPayload> CreateTodoItem(CreateTodoItemCommand input, [Service] ISender sender, [Service] ITopicEventSender topicEventSender)
+    {
+        var result = await sender.Send(input);
+        await topicEventSender.SendAsync(nameof(CreateTodoItem), new TodoItemCreatedPayload(result.TodoItem.Id));
+        return result;
+    }
 
     public async Task<DeleteTodoItemPayload> DeleteTodoItem(DeleteTodoItemCommand input, [Service] ISender sender) => await sender.Send(input);
 
